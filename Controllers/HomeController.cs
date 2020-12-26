8b3958508276c9cd264e44bc9f7b458f40d96ece
Spirit5736 <kozhevnikovi@mail.ru>
2020-12-26 19:42:52 +0700

    initial commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
 
 namespace SportsStore.Controllers
 {
+
     public class HomeController : Controller
     {
         private IRepository repository;
+
         public HomeController(IRepository repo) => repository = repo;
+
         public IActionResult Index() => View(repository.Products);
+
         [HttpPost]
         public IActionResult AddProduct(Product product)
         {
